1008f4ca821f193e515e853856b68e90e9036e20
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-08 22:55:44 +0500

    feat: adding modal form for the project

--- todo-app/src/components/TodoList/TodoList.jsx
+++ todo-app/src/components/TodoList/TodoList.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "./TodoList.scss";
 
 const TodoList = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <header className="header">
       <h1 className="main-title">Todo List</h1>
-      <button className="btn btn-primary">Create todo</button>
+      <button className="btn btn-primary" onClick={toggle}>
+        Create todo
+      </button>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi incidunt
+          nobis fugiat, assumenda perspiciatis dignissimos repudiandae iusto
+          quos corrupti provident.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            OK
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </header>
   );
 };
