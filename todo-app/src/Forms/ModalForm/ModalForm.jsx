0e56fee9552529d917af4a51fb3893ef8c5c5b2c
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-08 23:20:15 +0500

    feat: adding new elements from reactstrap library

--- todo-app/src/Forms/ModalForm/ModalForm.jsx
+++ todo-app/src/Forms/ModalForm/ModalForm.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { Form, FormGroup, Label, Input } from "reactstrap";
 
 const ModalForm = ({ modal, toggle }) => {
   return (
     <Modal isOpen={modal} toggle={toggle}>
       <ModalHeader toggle={toggle}>Create Task</ModalHeader>
       <ModalBody>
-        <form>
-          <div className="form-group">
-            <input type="text" placeholder="Enter task category here" />
-          </div>
-          <div className="form-group">
-            <textarea placeholder="Enter task description here"></textarea>
-          </div>
-        </form>
+        <Form>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="with a placeholder"
+              type="email"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Text Area</Label>
+            <Input id="exampleText" name="text" type="textarea" />
+          </FormGroup>
+        </Form>
       </ModalBody>
       <ModalFooter>
         <Button color="success" onClick={toggle}>
