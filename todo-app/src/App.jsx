1c6561d960dc7d7c2e2e1151a8f5421b54e66d46
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-09 23:26:06 +0500

    feat: adding new functionality

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -5,23 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const [task, setTask] = useState(["sss"]);
-
-  const test = () => {
-    console.log(task);
-    task.push("item");
-  };
+  const [task, setTask] = useState([]);
 
   return (
     <main>
       <TodoList task={task} />
       <TodoContainer task={task} />
-      <ul>
-        {task.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-      <button onClick={test}>+</button>
     </main>
   );
 }
